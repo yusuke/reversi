21eaae54be63ff3d34450782d14f75e34a195339
Yusuke Yamamoto <yusuke@mac.com>
2010-06-06 01:58:36 +0900

    corrected grid paits

--- src/main/java/jp/samuraism/reversi/ReversiApplet.java
+++ src/main/java/jp/samuraism/reversi/ReversiApplet.java
@@ -40,7 +40,6 @@ public class ReversiApplet extends Applet {
     private Image white, black, blank;
     private int cellWidth, cellHeight, width, height;
     private Image turn;
-    private Image buffer;
 
     public void init() {
         white = getImage(getDocumentBase(), getParameter("white"));
@@ -53,7 +52,6 @@ public class ReversiApplet extends Applet {
         cellHeight = (d.height - 9) / 8;
         width = d.width;
         height = d.height;
-        buffer = createImage(width, height);
         addMouseListener(ma);
         log("initializing the board");
         for (int i = 0; i < 8; i++) {
@@ -73,23 +71,18 @@ public class ReversiApplet extends Applet {
         public void mousePressed(MouseEvent me) {
             Point p = me.getPoint();
             if (place(p.x / (cellWidth + 1), p.y / (cellHeight + 1), turn)) {
-                log("ok");
                 repaint();
                 turn = getReversedColor(turn);
-                if (!isPlacable(turn)) {
-                    log("1");
-                    if (!isPlacable(getReversedColor(turn))) {
-                        // both players are not placable
-                        log("2");
+                if (!isPlaceable(turn)) {
+                    if (!isPlaceable(getReversedColor(turn))) {
+                        // both players are not placeable
                         judge();
                     } else {
                         // pass
-                        log("3");
                         turn = getReversedColor(turn);
                     }
                 }
             } else {
-                log("invalid axis");
             }
         }
     };
@@ -97,29 +90,31 @@ public class ReversiApplet extends Applet {
     public void paint(Graphics g) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                buffer.getGraphics().drawImage(board[i][j], 1 + (cellWidth + 1) * i,
-                        1 + (cellHeight + 1) * j, cellWidth, cellHeight, this);
+                g.drawImage(board[i][j], 1 + (cellWidth + 1) * i + i,
+                        1 + (cellHeight + 1) * j + j, cellWidth, cellHeight, this);
             }
         }
 
-        buffer.getGraphics().setColor(Color.black);
-        for (int i = 0; i <= 8; i++) {
-            buffer.getGraphics().drawLine(0, i * (cellHeight + 1), width - 1, i * (cellHeight + 1));
-            buffer.getGraphics().drawLine(i * (cellWidth + 1), 0, i * (cellWidth + 1), height - 1);
+        g.setColor(Color.black);
+        for (int i = 0; i < 9; i++) {
+            g.drawLine(0, i * (cellHeight + 1) + i, width, i * (cellHeight + 1) + i);
+            g.drawLine(i * (cellWidth + 1) + i, 0, i * (cellWidth + 1) + i, height);
         }
-
-        g.drawImage(buffer, 0, 0, this);
     }
 
     public void update(Graphics g) {
         paint(g);
     }
-    //置ける場所が一カ所でもあるか判定するメソッド
 
-    public boolean isPlacable(Image state) {
+    /**
+     * 置ける場所が一カ所でもあるか判定する
+     * @param state 色
+     * @return 置けるかどうか
+     */
+    public boolean isPlaceable(Image state) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (canPutKoma(i, j, state) != 0) {
+                if (searchPlaceableDirection(i, j, state) != 0) {
                     return true;
                 }
             }
@@ -142,12 +137,12 @@ public class ReversiApplet extends Applet {
     }
 
     /**
-     * 反対の色を返す
+     * 色を反転させる
      *
-     * @param state
-     * @return
+     * @param state 反転させる色
+     * @return 反転した色
      */
-    Image getReversedColor(Image state) {
+    private Image getReversedColor(Image state) {
         if (state == white) {
             return black;
         } else if (state == black) {
@@ -165,12 +160,11 @@ public class ReversiApplet extends Applet {
      * @param state state
      * @return if the placement succeeded
      */
-
-    boolean place(int x, int y, Image state) {
+    private boolean place(int x, int y, Image state) {
         byte search = 1;
         byte direction;
         //駒を置けるかどうかの判定
-        if ((direction = canPutKoma(x, y, state)) != 0) {
+        if ((direction = searchPlaceableDirection(x, y, state)) != 0) {
             //i,j = 探索するベクトル
             for (int i = -1; i <= 1; i++) {
                 for (int j = -1; j <= 1; j++) {
@@ -194,33 +188,33 @@ public class ReversiApplet extends Applet {
     /**
      * 駒を置けるかどうかの判定
      *
-     * @param x
-     * @param y
+     * @param x x座標
+     * @param y y座標
      * @param state 置く色
      * @return byteで駒を置けるかどうか ビット毎に置ける方向を示す 0:左上 1:左  2:左下  3:上  4:下  5:右上  6:右  7:右下
      */
-    public byte canPutKoma(int x, int y, Image state) {
+    public byte searchPlaceableDirection(int x, int y, Image state) {
         byte direction = 0;
         byte search = 1;
         if (board[x][y] == blank) {
-            //i,j:探索するベクトル
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    if (isInGrid(x + i, y + j)) {
+            for (int xx = -1; xx <= 1; xx++) {
+                for (int yy = -1; yy <= 1; yy++) {
+                    // xx, yy :探索するベクトル
+                    if (isInsideBoard(x + xx, y + yy)) {
                         ///一つとなりに置く駒と違う色があるか
-                        if ((board[x + i][y + j] != blank) && (board[x + i][y + j] != state)) {
-                            //さらにその先に置く駒の色があるか
-                            for (int k = 2; isInGrid(x + i * k, y + j * k); k++) {
-                                if (board[x + i * k][y + j * k] == state) {
-                                    //石を置ける！
+                        if ((board[x + xx][y + yy] != blank) && (board[x + xx][y + yy] != state)) {
+                            //さらにその先に置く駒の色があるか盤面上を探索
+                            for (int k = 2; isInsideBoard(x + xx * k, y + yy * k); k++) {
+                                if (board[x + xx * k][y + yy * k] == state) {
+                                    //自分の色があった、石を置ける！
                                     direction |= search;
-                                } else if (board[x + i * k][y + j * k] == blank) {
+                                } else if (board[x + xx * k][y + yy * k] == blank) {
                                     break;
                                 }
                             }
                         }
                     }
-                    if (!((i == 0) && (j == 0))) {
+                    if (!((xx == 0) && (yy == 0))) {
                         search <<= 1;
                     }
                 }
@@ -233,9 +227,13 @@ public class ReversiApplet extends Applet {
         }
     }
 
-    //座標が盤の中に収まっているかどうか
-
-    boolean isInGrid(int x, int y) {
+    /**
+     * 座標が盤の中に収まっているかどうか
+     * @param x x座標
+     * @param y y座標
+     * @return 収まっているかどうか
+     */
+    private boolean isInsideBoard(int x, int y) {
         return (x >= 0) && (x < board.length) & (y >= 0) && (y < board[0].length);
     }
 
